b56bb5ffed9556a01087d8b2b4d307011417e015
shahzadnit <shahzadnit@gmail.com>
2019-10-02 11:58:09 +0530

    .

---
+++ 22_image_pyramids_opencv.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+img = cv2.imread("lena.jpg")
+layer = img.copy()
+gaussian_pyramid_list = [layer]
+
+for i in range(6):
+    layer = cv2.pyrDown(layer)
+    gaussian_pyramid_list.append(layer)
+    #cv2.imshow(str(i), layer)
+
+layer = gaussian_pyramid_list[5]
+cv2.imshow('upper level Gaussian Pyramid', layer)
+laplacian_pyramid_list = [layer]
+
+for i in range(5, 0, -1):
+    gaussian_extended = cv2.pyrUp(gaussian_pyramid_list[i])
+    laplacian = cv2.subtract(gaussian_pyramid_list[i-1], gaussian_extended)
+    cv2.imshow(str(i), laplacian)
+
+cv2.imshow("Original image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ 7_merge_split_img.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+img = cv2.imread("messi5.jpg")
+img2 = cv2.imread("OpenCV_Logo.png")
+b,g,r=cv2.split(img)
+img = cv2.merge((b,g,r))
+ball = img[280:340,330:390]
+img[273:333,100:160]= ball
+img =cv2.resize(img,(512,512))
+img2 =cv2.resize(img2,(512,512))
+
+# dst= cv2.add(img,img2)
+dst= cv2.addWeighted(img,.7,img2,.3,0)
+cv2.imshow('image',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
